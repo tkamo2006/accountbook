35ef9b52b3664e655aef8271eb2811e9af8ab6f6
yeahxne <dpdnjs0125@naver.com>
2022-08-06 20:09:26 +0900

    Initial commit

---
+++ src/test/java/lookie/accountbook/AccountbookApplicationTests.java
@@ -0,0 +1,13 @@
+package lookie.accountbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
